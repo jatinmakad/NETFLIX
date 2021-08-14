d3cd81ba3163207b4d6ab5fa06e80acf5a6039f7
jatinmakad <38394914+jatinmakad@users.noreply.github.com>
2021-08-14 13:15:54 +0530

    ll

--- src/Banner.js
+++ src/Banner.js
@@ -24,7 +24,7 @@ function Banner() {
            style={{
                backgroundSize:"cover",
                backgroundImage:`url(
-                "https://image.tmdb.org/t/p/original/${movie ?.backdrop_path}"
+                "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
                )`,
                backgroundPosition:"center center",
            }}
